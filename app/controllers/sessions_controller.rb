6b6c18fefcd78c1a941c553ee2ea67d8c6725be9
DianaTavares <dianab_lopez@hotmail.com>
2017-08-21 10:38:22 -0500

    iniciamos con proyecto

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      #operador ternario que guarda la informacion del usuario en una cookie permanente para recordad al usuario en el borwser si el asi lo decea seleccionando la remember_me check_box
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+end
