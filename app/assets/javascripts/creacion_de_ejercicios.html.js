0c348d3a2af730049396e63019b6d4c12fb13705
doberman7 <ivanrubioangeles@gmail.com>
2017-08-22 16:41:28 -0500

    boton para borrar area de trabajo

--- app/assets/javascripts/creacion_de_ejercicios.html.js
+++ app/assets/javascripts/creacion_de_ejercicios.html.js
@@ -1,9 +1,9 @@
 $(document).on('click','#btn_inputs', function() {
 
-  console.log("en creacion_de_ejercicios.html.js se ha dado click al boton con id #btn_inputs");
+      console.log("en creacion_de_ejercicios.html.js se ha dado click al boton con id #btn_inputs");
     // $("#btn_inputs").click(function(){
       //create an element
-      var $element = $('<div class="draggableResizable" />');
+      var $element = $('<div class="draggableResizable"/>').append('<input name="input-draggable" />este es un input MotherFuck</a>');
 
       //append it to the DOM
       $("#work_area").append($element);
