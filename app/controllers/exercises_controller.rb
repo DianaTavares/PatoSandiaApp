a28415bf8dbe8be6d508acd75dd262e1ff4d7434
doberman7 <ivanrubioangeles@gmail.com>
2017-08-29 00:03:53 -0500

    aparente problema con k_index resuelto

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -43,45 +43,27 @@ class ExercisesController < ApplicationController
           posiciones_keys = params[:positions_ed].keys
           k_index = 0
           inputs.each_with_index do |input, index|
-            #  p "-" * 50
-            # cada input es un Ary["si es Text o input", "el valor del mismo"]
-            # objener el value de cada posicion, a través del Arry posiciones_keys, el cual es convertido en simbolo, ya que es un string en el Array
-            #  y asignar top
-            top =   params[:positions_ed][posiciones_keys[k_index].to_sym]
-            # sumar uno a la llave del index para buscar el siguiente param
-            k_index = k_index + 1
-            # extraer valores del left del pararam positions
-            left =  params[:positions_ed][posiciones_keys[k_index].to_sym]
-            k_index = k_index + 1
+            p "+" * 50
+            
 
-            # p "*" * 50
+            if params[:positions_ed].length > k_index
+              p input
+              p top =   params[:positions_ed][posiciones_keys[k_index].to_sym]
+              # sumar uno a la llave del index para buscar el siguiente param
+               k_index = k_index + 1
+              # extraer valores del left del pararam positions
+              p left =  params[:positions_ed][posiciones_keys[k_index].to_sym]
+                k_index = k_index + 1
+              #  p "-" * 50
+            end
             # determinar si se trata de un un input o un text, cada ary contiene d elementos, el primer es que dice Text o input
             # si inicia con i es input
             if input[0].chr == "i"
-              p input
-              # params[:exercise_edit_ids_INPUT].values.each do |id_inp|
-              #   p inp.id if inp.id == id_inp.to_i
-              # end
-              Input.where(exercise_id: $exercise_id_edit).each_with_index do |inp, ind|
-                p "-" * 50
-                p inp.id
-                # input_ob = Input.new(answer: input.last, x_position: left.to_i, y_position: top.to_i)
-                p "-" * 50
-              end
-              # si inicia con t es texto
+              # # crear elobjeto y agregar atributos
+              # input_ob = Input.new(answer: input.last, x_position: left.to_i, y_position: top.to_i)
+              # # si inicia con t es texto
             elsif input[0].chr ==  "t"
-              p input
-              # p 'left'
-              # p left.to_i
-              # p 'top'
-              # p top.to_i
-              Text.where(exercise_id: $exercise_id_edit).each_with_index do |tex,ind|
-                p tex.id
-                #  params[:exercise_edit_ids_TEXT].values.each do |id_text|
-                #    p tex.id if tex.id == id_text.to_i
-                #  end
-                 # input_ob = Text.new(text: input.last, x_position: left.to_i, y_position: top.to_i)
-              end
+              # input_ob = Text.new(text: input.last, x_position: left.to_i, y_position: top.to_i)
             end
             # input_ob.update(exercise_id: $exercise_id_edit)
             # p input_ob.save
