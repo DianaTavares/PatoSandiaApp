ea800b60b65547e9d9f1671d244028b6774bf08f
DianaTavares <dianab_lopez@hotmail.com>
2017-08-29 20:22:59 -0500

    aun agregando las ediciones

--- app/assets/javascripts/creacion_de_ejercicios.html.js
+++ app/assets/javascripts/creacion_de_ejercicios.html.js
@@ -3,59 +3,9 @@ var $inputN = 0
 var $array = new Array
 var $arrayEdit = new Array
 var $textN = 0
+//contador de la posición de array
 var $arrayN = -1
 $(document).ready(function() {
-  // event handler al apretar botono "recuperar ubicaciones "
-  $(document).on('click','#btn_recover', function() {
-    $.each($(".editables "), function( indexInput, input ) {
-        // el index comenzarlo en 1
-        indexInput++
-        // apuntar al input donde se coloca el inputText
-        var $text_edit =  $('#text_edit_'+indexInput+'');
-        // si no es indefinido, es decir que sí existe
-        if ($text_edit != undefined) {
-          // evitar que el draggable afecte a los demás text
-          $($text_edit).attr("style","position: absolute")
-          //  obtener coordenadas TEXT
-          var $pxT= $('#pxT-'+indexInput+'-edit').html();
-          var $pyT= $('#pyT-'+indexInput+'-edit').html();
-          // establecer la ubicación del inputText
-          $($text_edit).offset({top:($pyT),left:($pxT)})
-          // establecer atributo clase
-          $($text_edit).attr("class",'editable-text');
-          // hacer draggable los texts
-          $($text_edit).draggable({
-            // manterlo limitado al área de trabajo
-            containment: '#work_area',
-          }).resizable();
-          $arrayEdit.push($text_edit);
-        }
-        // apuntar al input donde se colocan las respuestas
-        var $input_edit =  $('#input_edit_'+indexInput+'');
-        // si no existe alguno..
-        if ($input_edit != undefined) {
-          // evitar que el draggable afecte a los demás inpus
-          $($input_edit).attr("style","position: absolute")
-          //  obtener coordenadas INPUT
-          var $pxI= $('#pxI-'+indexInput+'-edit').html();
-          var $pyI= $('#pyI-'+indexInput+'-edit').html();
-          // colcar en cooordenadas
-          $($input_edit).offset({top:($pyI),left:($pxI)})
-          // agregar clase
-          $($input_edit).attr("class",'editable-input');
-          // hacer dragable los inputs
-          $($input_edit).draggable({
-            // manterlo limitado al área de trabajo
-            containment: '#work_area',
-          }).resizable();
-          $arrayEdit.push($input_edit);
-        }
-
-
-    });
-
-  });
-
   $(document).on('click','#btn_delete', function() {
     $inputN = 0
     $array = new Array
@@ -71,7 +21,7 @@ $(document).ready(function() {
     //console.log($inputN);
     // console.log("en creacion_de_ejercicios.html.js se ha dado click al boton con id #btn_inputs");
     // asignar al un div draggableResizable y anexarle un label  y name para que el botón "terminar trabajo" envíe los datos al params
-    var $element = $('<div id="'+$arrayN+'" class="draggableResizable draggableinput" style="position: absolute"/>').append('<label for="exercise_input"><input id="input-'+$inputN+'" type= text name="exercise[input'+$inputN+']"/><p> <a class="close" href="#">x</a> <br> input</p> </label>');
+    var $element = $('<div id="'+$arrayN+'" class="draggableResizable draggableinput" style="position: absolute"/>').append('<label for="exercise_input"><input id="input-'+$inputN+'" type= text name="exercise[input'+$inputN+']"/>Input <a class="close" href="#">x</a> </label>');
     // console.log($element);
     $array.push($element);
     //append it to the DOM
@@ -100,7 +50,7 @@ $(document).ready(function() {
     $("#work_area").on("click",".close",function(event){
         //paramos el envio del link
         event.preventDefault();
-        //giardamos en la variable delete el div, que es el padre general del elemento .close
+        //guardamos en la variable delete el div, que es el padre general del elemento .close
         var deleted = $(this).parent().parent().parent();
         //lo removemos del DOM
         deleted.remove();
@@ -116,7 +66,7 @@ $(document).on('click','#btn_text', function() {
       $arrayN++
       // console.log("en creacion_de_ejercicios.html.js se ha dado click al boton con id #btn_inputs");
       // asignar al un div draggableResizable y anexarle un label  y name para que el botón "terminar trabajo" envíe los datos al params
-      var $element = $('<div id="'+$arrayN+'" class="draggableResizable draggabletext" style="position: absolute" />').append('<label for="exercise_input"><input id="text-'+$textN+'" type= text name="exercise[text'+$textN+']" /><p> <a class="close" href="#">x</a> <br> texto</p></label>');
+      var $element = $('<div id="'+$arrayN+'" class="draggableResizable draggabletext" style="position: absolute" />').append('<label for="exercise_input"><input id="text-'+$textN+'" type= text name="exercise[text'+$textN+']" />Texto <a class="close" href="#">x</a></label>');
       // console.log($element);
       $array.push($element);
       //append it to the DOM
@@ -142,7 +92,7 @@ $(document).on('click','#btn_text', function() {
     //funcion para eliminar textos del área de trabajo
     $("#work_area").on("click",".close",function(event){
         event.preventDefault();
-        var deleted = $(this).parent().parent().parent();
+        var deleted = $(this).parent().parent();
         deleted.remove();
         $array[deleted.attr("id")] = 0;
       });
